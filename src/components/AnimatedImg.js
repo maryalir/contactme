d1ec99efa174a13b5d954622567106fbdfab5453
maryalir <maryalir@gmail.com>
2020-05-05 22:00:39 -0400

    looking good, need solve load problem

--- src/components/AnimatedImg.js
+++ src/components/AnimatedImg.js
@@ -6,31 +6,32 @@ import './AnimatedImg.css';
 
 const AnimatedImg = () => {
 
-  let a = ([require ('../images/dosa.jpeg'),require('../images/unoa.jpeg')]);
-  var b = [require ('../images/dosb.jpeg'), require ('../images/unob.jpeg')];
-  var c = [require ('../images/dosc.jpeg'), require ('../images/unoc.jpeg')];
+  let a = [require ('../images/dosa.jpeg'),require('../images/unoa.jpeg'), require ('../images/unoc.jpeg')];
+  let b = [require ('../images/dosc.jpeg'),require ('../images/unob.jpeg'), require ('../images/dosb.jpeg')];
+  // var c = [require ('../images/dosc.jpeg'), require ('../images/unoc.jpeg')];
   var i=0;
+
 
   function imgChange(){
 
-    if (i===a.length-1){
-      i=0;
-    } else{ 
-      i=i+1;
-    }
-    setTimeout(() => {
-      document.getElementById("imguno").src=a[i]; 
+      if (i===a.length-1){
+        i=0;
+      } else{ 
+        i=i+1;
+      }
+      setTimeout(() => {
+        document.getElementById("imguno").src=a[i]; 
+      }, 1500);
 
-    }, 1500);
+      setTimeout(() => {
+        document.getElementById("imgdos").src=b[i]; 
+        imgChange();
+      }, 3000);
 
-    setTimeout(() => {
-      document.getElementById("imgdos").src=b[i]; 
-    }, 3000)
-
-    setTimeout(() => {
-      document.getElementById("imgtres").src=c[i]; 
-      imgChange();
-    },4500)
+    // setTimeout(() => {
+    //   document.getElementById("imgtres").src=c[i]; 
+    //   imgChange();
+    // },4500);
   }
 
   return(
@@ -44,9 +45,9 @@ const AnimatedImg = () => {
       <img className='imgdos'  id='imgdos' src='' alt=""/>
     </div>
 
-    <div className='divimgtres'>
+    {/* <div className='divimgtres'>
       <img className='imgtres'  id='imgtres' src='' alt=""/>
-    </div>
+    </div> */}
 
     </div>
   )
